06e56a1f905c36d64454c16333e8e943db431234
Carl Wieland <cwieland@esri.com>
2020-01-08 10:38:24 -0500

    sort and minmax

--- mtlsort/Shader.metal
+++ mtlsort/Shader.metal
@@ -72,7 +72,8 @@ kernel void pointBitonic(device Vector2 *input [[buffer(0)]],
 
 kernel void boundingBox(const device Vector2 *array [[ buffer(0) ]],
                         volatile device atomic_int *result [[ buffer(1) ]],
-                        threadgroup int *shared_memory [[threadgroup(0)]],
+                        threadgroup int *shared_X_memory [[threadgroup(0)]],
+                        threadgroup int *shared_Y_memory [[threadgroup(1)]],
                         uint id [[ thread_position_in_grid ]],
                         uint tid [[ thread_index_in_threadgroup ]],
                         uint bid [[ threadgroup_position_in_grid ]],
@@ -80,30 +81,30 @@ kernel void boundingBox(const device Vector2 *array [[ buffer(0) ]],
 
 
     uint i = bid * blockDim + tid;
-    shared_memory[tid] = i;
-    shared_memory[tid + blockDim] = i;
+    shared_X_memory[tid] = i;
+    shared_Y_memory[tid] = i;
     threadgroup_barrier(mem_flags::mem_threadgroup);
 
     for (uint stride = 1; stride < blockDim; stride *= 2) {
         // only accept increasing multiples of two
         if (tid % (2 * stride) == 0) {
-            if ( array[shared_memory[tid + stride]].x < array[shared_memory[tid]].x) {
-                shared_memory[tid] = shared_memory[tid + stride];
+            if ( array[shared_X_memory[tid + stride]].x < array[shared_X_memory[tid]].x) {
+                shared_X_memory[tid] = shared_X_memory[tid + stride];
             }
-            if ( array[shared_memory[tid + stride + blockDim]].y < array[shared_memory[tid + blockDim]].y) {
-                shared_memory[tid + blockDim] = shared_memory[tid + stride + blockDim];
+            if ( array[shared_Y_memory[tid + stride]].y < array[shared_Y_memory[tid]].y) {
+                shared_Y_memory[tid] = shared_Y_memory[tid + stride];
             }
         }
         threadgroup_barrier(mem_flags::mem_threadgroup);
 
         if (stride > 1 && tid % (2 * stride) == 1) {
             // Max calculations
-            if (array[shared_memory[tid + stride]].x > array[shared_memory[tid]].x) {
-                shared_memory[tid] = shared_memory[tid + stride];
+            if (array[shared_X_memory[tid + stride]].x > array[shared_X_memory[tid]].x) {
+                shared_X_memory[tid] = shared_X_memory[tid + stride];
             }
 
-            if ( array[shared_memory[tid + stride + blockDim]].y > array[shared_memory[tid + blockDim]].y) {
-                shared_memory[tid + blockDim] = shared_memory[tid + stride + blockDim];
+            if ( array[shared_Y_memory[tid + stride]].y > array[shared_Y_memory[tid]].y) {
+                shared_Y_memory[tid] = shared_Y_memory[tid + stride];
             }
 
         }
@@ -115,20 +116,20 @@ kernel void boundingBox(const device Vector2 *array [[ buffer(0) ]],
     if (0 == tid ) {
         int currentXMin = atomic_load_explicit(&result[0], memory_order_relaxed);
 
-        if( currentXMin == -1 || array[shared_memory[tid]].x < array[currentXMin].x) {
-            while(!atomic_compare_exchange_weak_explicit(&result[0], &currentXMin, shared_memory[tid], memory_order_relaxed, memory_order_relaxed)) {
+        if( currentXMin == -1 || array[shared_X_memory[tid]].x < array[currentXMin].x) {
+            while(!atomic_compare_exchange_weak_explicit(&result[0], &currentXMin, shared_X_memory[tid], memory_order_relaxed, memory_order_relaxed)) {
                 // another thread won. Check if we still need to set it.
-                if (array[shared_memory[tid]].x > array[currentXMin].x) {
+                if (array[shared_X_memory[tid]].x > array[currentXMin].x) {
                     break;
                 }
             }
         }
         int currentYMin = atomic_load_explicit(&result[1], memory_order_relaxed);
 
-        if( currentYMin == -1 ||  array[shared_memory[tid + blockDim]].y < array[currentYMin].y) {
-            while(!atomic_compare_exchange_weak_explicit(&result[1], &currentYMin, shared_memory[tid + blockDim], memory_order_relaxed, memory_order_relaxed)) {
+        if( currentYMin == -1 ||  array[shared_Y_memory[tid]].y < array[currentYMin].y) {
+            while(!atomic_compare_exchange_weak_explicit(&result[1], &currentYMin, shared_Y_memory[tid], memory_order_relaxed, memory_order_relaxed)) {
                 // another thread won. Check if we still need to set it.
-                if (array[shared_memory[tid + blockDim]].y > array[currentYMin].y) {
+                if (array[shared_Y_memory[tid]].y > array[currentYMin].y) {
                     break;
                 }
             }
@@ -137,10 +138,10 @@ kernel void boundingBox(const device Vector2 *array [[ buffer(0) ]],
 
         int currentXMaxIndex = atomic_load_explicit(&result[2], memory_order_relaxed);
 
-        if(currentXMaxIndex == -1 || array[shared_memory[tid]].x > array[currentXMaxIndex].x) {
-            while(!atomic_compare_exchange_weak_explicit(&result[2], &currentXMaxIndex, shared_memory[tid], memory_order_relaxed, memory_order_relaxed)) {
+        if(currentXMaxIndex == -1 || array[shared_X_memory[tid]].x > array[currentXMaxIndex].x) {
+            while(!atomic_compare_exchange_weak_explicit(&result[2], &currentXMaxIndex, shared_X_memory[tid], memory_order_relaxed, memory_order_relaxed)) {
                 // another thread won. Check if we still need to set it.
-                if (array[shared_memory[tid]].x < array[currentXMaxIndex].x) {
+                if (array[shared_X_memory[tid]].x < array[currentXMaxIndex].x) {
                     break;
                 }
             }
@@ -148,10 +149,10 @@ kernel void boundingBox(const device Vector2 *array [[ buffer(0) ]],
 
         int currentYMax = atomic_load_explicit(&result[3], memory_order_relaxed);
 
-        if(currentYMax == -1 || array[shared_memory[tid + blockDim]].y > array[currentYMax].y) {
-            while(!atomic_compare_exchange_weak_explicit(&result[3], &currentYMax, shared_memory[tid + blockDim], memory_order_relaxed, memory_order_relaxed)) {
+        if(currentYMax == -1 || array[shared_Y_memory[tid]].y > array[currentYMax].y) {
+            while(!atomic_compare_exchange_weak_explicit(&result[3], &currentYMax, shared_Y_memory[tid], memory_order_relaxed, memory_order_relaxed)) {
                 // another thread won. Check if we still need to set it.
-                if (array[shared_memory[tid + blockDim]].y < array[currentYMax].y) {
+                if (array[shared_Y_memory[tid]].y < array[currentYMax].y) {
                     break;
                 }
             }
--- mtlsort/main.swift
+++ mtlsort/main.swift
@@ -84,10 +84,6 @@ func sortData() {
     }
     print("shift: \(shift) Match:\(match)")
 
-
-
-
-
 }
 
 func boundingBox() {
@@ -96,10 +92,10 @@ func boundingBox() {
 
     let commandQueue = device.makeCommandQueue()!
     let library = device.makeDefaultLibrary()!
-    let sortFunction = library.makeFunction(name: "boundingBox")!
-    let pipeline = try! device.makeComputePipelineState(function: sortFunction)
+    let bondingBoxFunction = library.makeFunction(name: "boundingBox")!
+    let boundingBoxPipelineState = try! device.makeComputePipelineState(function: bondingBoxFunction)
 
-    let shift = 8
+    let shift = 20
     let size = 1<<shift
 
     typealias DataType = Vector2
@@ -113,10 +109,11 @@ func boundingBox() {
         }
     }
 
+    var start = Date()
     let dataBuffer1 = device.makeBuffer(bytes: &data, length: MemoryLayout<DataType>.stride * data.count, options: [.cpuCacheModeWriteCombined])!
 
-    let threadgroupsPerGrid = MTLSize(width: size/pipeline.threadExecutionWidth, height: 1, depth: 1)
-    let threadsPerThreadgroup = MTLSize(width: pipeline.threadExecutionWidth, height: 1, depth: 1)
+    let threadgroupsPerGrid = MTLSize(width: size/boundingBoxPipelineState.threadExecutionWidth, height: 1, depth: 1)
+    let threadsPerThreadgroup = MTLSize(width: boundingBoxPipelineState.threadExecutionWidth, height: 1, depth: 1)
 
     var resultSize: [Int32] = [-1,-1,-1,-1]
     let resultBuf = device.makeBuffer(bytes:&resultSize, length: MemoryLayout<Int32>.stride * resultSize.count, options: [.storageModeManaged])!
@@ -124,10 +121,11 @@ func boundingBox() {
     let commands = commandQueue.makeCommandBuffer()!
 
     let encoder = commands.makeComputeCommandEncoder()!
-    encoder.setComputePipelineState(pipeline)
+    encoder.setComputePipelineState(boundingBoxPipelineState)
     encoder.setBuffer(dataBuffer1, offset: 0, index: 0)
     encoder.setBuffer(resultBuf, offset: 0, index: 1)
-    encoder.setThreadgroupMemoryLength((pipeline.threadExecutionWidth * 2 * MemoryLayout<Int32>.stride), index: 0)
+    encoder.setThreadgroupMemoryLength(boundingBoxPipelineState.threadExecutionWidth * MemoryLayout<Int32>.stride, index: 0)
+    encoder.setThreadgroupMemoryLength(boundingBoxPipelineState.threadExecutionWidth * MemoryLayout<Int32>.stride, index: 1)
     encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
     encoder.endEncoding()
     if let blitEncoder = commands.makeBlitCommandEncoder() {
@@ -135,24 +133,177 @@ func boundingBox() {
         blitEncoder.endEncoding()
     }
 
-    var start = Date()
     commands.commit()
     commands.waitUntilCompleted()
 
-    let dataPointer = resultBuf.contents().assumingMemoryBound(to: Int32.self)
-    let dataBufferPointer = UnsafeMutableBufferPointer(start: dataPointer, count: 4)
-    let resultsArray = Array.init(dataBufferPointer)
+    let boundingResults = resultBuf.contents().assumingMemoryBound(to: Int32.self)
+    let boundingDataBuffer = UnsafeMutableBufferPointer(start: boundingResults, count: 4)
+    let boundingResultArray = Array(boundingDataBuffer)
     var stop = Date()
     var time =  stop.timeIntervalSince(start)
     print("GPU Time:\(time)")
 
-    print("results:\(resultsArray)")
-    let minX = data[Int(resultsArray[0])].x
-    let minY = data[Int(resultsArray[1])].y
-    let maxX = data[Int(resultsArray[2])].x
-    let maxY = data[Int(resultsArray[3])].y
+    print("results:\(boundingResults)")
+    var minX = data[Int(boundingResultArray[0])].x
+    var minY = data[Int(boundingResultArray[1])].y
+    var maxX = data[Int(boundingResultArray[2])].x
+    var maxY = data[Int(boundingResultArray[3])].y
 
-    print("Bounding box:(\(minX),\(minY)), (\(maxX),\(maxY))")
+    print("GPU Bounding box:(\(minX),\(minY)), (\(maxX),\(maxY))")
+
+    start = Date()
+    minX = data[0].x
+    minY = data[0].y
+    maxX = data[0].x
+    maxY = data[0].y
+    data.forEach {
+        minX = min($0.x, minX)
+        minY = min($0.y, minY)
+        maxX = max($0.x, maxX)
+        maxY = max($0.y, maxY)
+    }
+    stop = Date()
+    time =  stop.timeIntervalSince(start)
+    print("CPU Time:\(time)")
+    print("CPU Bounding box:(\(minX),\(minY)), (\(maxX),\(maxY))")
+
 }
 
-boundingBox()
+
+
+
+func doBoth() {
+    //let device = MTLCreateSystemDefaultDevice()!
+    let device = MTLCopyAllDevices()[1]
+
+    let commandQueue = device.makeCommandQueue()!
+    let library = device.makeDefaultLibrary()!
+
+    let sortFunction = library.makeFunction(name: "pointBitonic")!
+    let sortXPipeline = try! device.makeComputePipelineState(function: sortFunction)
+
+    let bondingBoxFunction = library.makeFunction(name: "boundingBox")!
+    let boundingBoxPipelineState = try! device.makeComputePipelineState(function: bondingBoxFunction)
+
+    let shift = 16
+    let size = 1<<shift
+
+    typealias DataType = Vector2
+
+    var data: [DataType ] = [DataType](repeating: Vector2(x: 0, y: 0), count: size)
+    let squareSize = 1<<(shift >> 1)
+    for y in 0..<squareSize {
+        for x in 0..<squareSize  {
+            data[(y * squareSize) + x].x = Int32(x)
+            data[(y * squareSize) + x].y = Int32(y)
+        }
+    }
+
+    var start = Date()
+    let dataBuffer1 = device.makeBuffer(bytes: &data, length: MemoryLayout<DataType>.stride * data.count, options: [.storageModeManaged])!
+    dataBuffer1.didModifyRange(0..<MemoryLayout<DataType>.stride * data.count)
+    let threadgroupsPerGrid = MTLSize(width: size/sortXPipeline.threadExecutionWidth, height: 1, depth: 1)
+    let threadsPerThreadgroup = MTLSize(width: sortXPipeline.threadExecutionWidth, height: 1, depth: 1)
+
+    let logn = shift
+    let commands = commandQueue.makeCommandBuffer()!
+
+    for i in 0..<logn {
+        for j in 0...i {
+
+            var p = UInt32(i)
+            var q = UInt32(j)
+
+            let encoder = commands.makeComputeCommandEncoder()!
+            encoder.setComputePipelineState(sortXPipeline)
+            encoder.setBuffer(dataBuffer1, offset: 0, index: 0)
+            encoder.setBytes(&p, length: MemoryLayout<UInt32>.stride, index: 1)
+            encoder.setBytes(&q, length: MemoryLayout<UInt32>.stride, index: 2)
+            encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
+            encoder.endEncoding()
+
+
+        }
+    }
+
+    var resultSize: [Int32] = [-1,-1,-1,-1]
+    let resultBuf = device.makeBuffer(bytes:&resultSize, length: MemoryLayout<Int32>.stride * resultSize.count, options: [.storageModeManaged])!
+    let encoder = commands.makeComputeCommandEncoder()!
+
+    encoder.setComputePipelineState(boundingBoxPipelineState)
+    encoder.setBuffer(dataBuffer1, offset: 0, index: 0)
+    encoder.setBuffer(resultBuf, offset: 0, index: 1)
+    encoder.setThreadgroupMemoryLength(boundingBoxPipelineState.threadExecutionWidth * MemoryLayout<Int32>.stride, index: 0)
+    encoder.setThreadgroupMemoryLength(boundingBoxPipelineState.threadExecutionWidth * MemoryLayout<Int32>.stride, index: 1)
+    encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
+    encoder.endEncoding()
+    if let blitEncoder = commands.makeBlitCommandEncoder() {
+        blitEncoder.synchronize(resource: resultBuf)
+        blitEncoder.endEncoding()
+    }
+
+
+    if let blitEncoder = commands.makeBlitCommandEncoder() {
+        blitEncoder.synchronize(resource: dataBuffer1)
+        blitEncoder.endEncoding()
+    }
+
+    commands.commit()
+    commands.waitUntilCompleted()
+
+    let dataPointer = dataBuffer1.contents().assumingMemoryBound(to: DataType.self)
+    let dataBufferPointer = UnsafeMutableBufferPointer(start: dataPointer, count: data.count)
+    let resultsArray = Array.init(dataBufferPointer)
+
+    let boundingResults = resultBuf.contents().assumingMemoryBound(to: Int32.self)
+    let boundingDataBuffer = UnsafeMutableBufferPointer(start: boundingResults, count: 4)
+    let boundingResultArray = Array(boundingDataBuffer)
+    var minX = resultsArray[Int(boundingResultArray[0])].x
+    var minY = resultsArray[Int(boundingResultArray[1])].y
+    var maxX = resultsArray[Int(boundingResultArray[2])].x
+    var maxY = resultsArray[Int(boundingResultArray[3])].y
+
+    var stop = Date()
+    var time =  stop.timeIntervalSince(start)
+    print("GPU Bounding box:(\(minX),\(minY)), (\(maxX),\(maxY))")
+
+    print("GPU Time:\(time)")
+    start = Date()
+    data.sort { (a, b) -> Bool in
+        if a.x < b.x {
+            return true;
+        } else if a.x > b.x {
+            return false;
+        } else {
+            return a.y < b.y;
+        }
+    }
+    minX = data[0].x
+    minY = data[0].y
+    maxX = data[0].x
+    maxY = data[0].y
+    data.forEach {
+        minX = min($0.x, minX)
+        minY = min($0.y, minY)
+        maxX = max($0.x, maxX)
+        maxY = max($0.y, maxY)
+    }
+    stop = Date()
+    time = stop.timeIntervalSince(start)
+    print("CPU Time:\(time)")
+
+    var match = true
+    for i in 0..<data.count where match {
+        match = resultsArray[i].x == data[i].x && resultsArray[i].y == data[i].y
+    }
+    print("shift: \(shift) Match:\(match)")
+
+
+
+
+}
+
+
+doBoth()
+
+
